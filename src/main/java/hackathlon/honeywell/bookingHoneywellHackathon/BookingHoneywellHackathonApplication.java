d800e4bd3ac4b2e5211820128b5ad2f60676c750
Debajyoti Kundu <babai.k.007@gmail.com>
2020-08-08 12:26:36 +0530

    Pushing bare structure

---
+++ src/main/java/hackathlon/honeywell/bookingHoneywellHackathon/BookingHoneywellHackathonApplication.java
@@ -0,0 +1,13 @@
+package hackathlon.honeywell.bookingHoneywellHackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingHoneywellHackathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingHoneywellHackathonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hackathlon/honeywell/bookingHoneywellHackathon/BookingHoneywellHackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package hackathlon.honeywell.bookingHoneywellHackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingHoneywellHackathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
