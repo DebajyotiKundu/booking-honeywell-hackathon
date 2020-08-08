b148151726b03df99ae8371cb4355628836701f9
Debajyoti Kundu <babai.k.007@gmail.com>
2020-08-08 15:27:27 +0530

    Committing some changes to Promo repository

--- src/main/java/hackathlon/honeywell/bookingHoneywellHackathon/domain/model/promo/PromoRepository.java
+++ src/main/java/hackathlon/honeywell/bookingHoneywellHackathon/domain/model/promo/PromoRepository.java
@@ -3,11 +3,12 @@ package hackathlon.honeywell.bookingHoneywellHackathon.domain.model.promo;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource
+@RepositoryRestResource(path = "promos")
 public interface PromoRepository extends JpaRepository<Promo, Long> {
-	List<Promo> findByPromoCode(String promoCode);
+	List<Promo> findByPromoCode(@Param("promoCode") String promoCode);
 
-	List<Promo> findByEventId(Long eventId);
+	List<Promo> findByEventId(@Param("eventId") Long eventId);
 }
